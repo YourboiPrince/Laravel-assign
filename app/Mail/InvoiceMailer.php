698b00181203c38d766b0181332f3b3319f8cfac
morio <princebandit2004@gmail.com>
2024-03-28 09:54:12 +0300

    final committ

---
+++ app/Mail/InvoiceMailer.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Invoice;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class InvoiceCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $invoice;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param Invoice $invoice
+     * @return void
+     */
+    public function __construct(Invoice $invoice)
+    {
+        $this->invoice = $invoice;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Invoice Created')->view('emails.invoice_created');
+    }
+}
